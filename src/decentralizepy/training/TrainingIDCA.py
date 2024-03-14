8252c395cca5e9e617e51c940177a15b605c316b
TicaGit <thibaud.trinca@epfl.ch>
2024-03-14 09:59:45 +0100

    refactor + add a plot + add one model sharing + add explore option

--- src/decentralizepy/training/TrainingIDCA.py
+++ src/decentralizepy/training/TrainingIDCA.py
@@ -31,6 +31,7 @@ class TrainingIDCA(Training):
         full_epochs="",
         batch_size="",
         shuffle="",
+        explore_models="",
     ):
         """
         Constructor
@@ -70,21 +71,116 @@ class TrainingIDCA(Training):
         self.machine_id = machine_id
         self.mapping = mapping
         self.rounds = utils.conditional_value(rounds, "", int(1))
-        self.full_epochs = utils.conditional_value(full_epochs, "", False)
+        self.full_epochs = utils.conditional_value(full_epochs, "", True)
         self.batch_size = utils.conditional_value(batch_size, "", int(1))
         self.shuffle = utils.conditional_value(shuffle, "", False)
+        self.explore_models = utils.conditional_value(explore_models, "", False)
 
-    def reset_optimizer(self, optimizer):
+    def train(self, dataset: Dataset, treshold: float = 0.0):
         """
-        Replace the current optimizer with a new one
+        One training iteration
 
         Parameters
         ----------
-        optimizer : torch.optim
-            A new optimizer
+        dataset : decentralizepy.datasets.Dataset
+            The training dataset. Should implement get_trainset(batch_size, shuffle)
+        treshold : float, optional
+            Treshold in [0, 1] to explore the space. If set to 0, no exploration is done.
 
         """
-        self.optimizer = optimizer
+
+        # chose the best model
+        self.choose_best_model(dataset, treshold=treshold)
+
+        # reset the optimizer to match the current model parameters
+        self.reset_optimizer(
+            self.optimizer_class(
+                self.current_model.parameters(), **self.optimizer_params
+            )
+        )
+
+        self.current_model.train()  # set the current model to train mode
+
+        if self.full_epochs:
+            self.train_full(dataset)
+        else:
+            self.train_partial_data(dataset)
+
+    def choose_best_model(self, dataset: Dataset, treshold: float = 0.0):
+        """
+        Choose the best model from the list of models.
+        Also, have a percentage of choosing a random model to explore the space.
+
+        Parameters
+        ----------
+        dataset : decentralizepy.datasets.Dataset
+            The dataset interface.
+        """
+        if self.explore_models:
+            if random.uniform(0, 1) < treshold:
+                # chosing a random model
+                self.current_model_idx = random.randint(0, len(self.models) - 1)
+                self.current_model_loss = None
+                self.models_losses = None
+                self.current_model = self.models[self.current_model_idx]
+                self.current_model_is_best = False
+                logging.info(f"Random model chosen:{self.current_model_idx}")
+                return
+        # chosing the best model
+        self.models_losses = [self.eval_loss(model, dataset) for model in self.models]
+        self.current_model_loss = min(self.models_losses)
+        self.current_model_idx = self.models_losses.index(self.current_model_loss)
+        self.current_model = self.models[self.current_model_idx]
+        self.current_model_is_best = True
+        logging.info(f"Best model chosen:{self.current_model_idx}")
+
+    def train_full(self, dataset: Dataset):
+        """
+        One training iteration, goes through the entire dataset
+
+        Parameters
+        ----------
+        trainset : torch.utils.data.Dataloader
+            The training dataset.
+
+        """
+        trainset = dataset.get_trainset(self.batch_size, self.shuffle)
+        for epoch in range(self.rounds):
+            epoch_loss = 0.0
+            count = 0
+            for data, target in trainset:
+                logging.debug(
+                    "Starting minibatch {} with num_samples: {}".format(
+                        count, len(data)
+                    )
+                )
+                logging.debug("Classes: {}".format(target))
+                epoch_loss += self._trainstep(data, target)
+                count += 1
+            logging.debug("Epoch: {} mean loss: {}".format(epoch, epoch_loss / count))
+
+    def train_partial_data(self, dataset: Dataset):
+        """Probably useless, don't use it. Trains on a subset of the data.
+
+        Args:
+            dataset (Dataset): the dataset holding the data
+        """
+        trainset = dataset.get_trainset(self.batch_size, self.shuffle)
+        for epoch in range(self.rounds):
+            count = 0
+            iter_loss = 0.0
+            for data, target in trainset:
+                iter_loss += self._trainstep(data, target)
+                count += 1
+                logging.debug(
+                    "Minibatch: {}, total mean loss: {}".format(
+                        count, iter_loss / count
+                    )
+                )
+                if count >= self.rounds:
+                    logging.debug(f"Finished epoch {epoch} after {count} minibatchs")
+                    count = 0
+                    break
 
     def eval_loss(self, model: Model, dataset: Dataset):
         """
@@ -110,7 +206,7 @@ class TrainingIDCA(Training):
         logging.info("Loss after iteration: {}".format(loss))
         return loss
 
-    def trainstep(self, data, target):
+    def _trainstep(self, data, target):
         """
         One training step on a minibatch.
 
@@ -127,108 +223,12 @@ class TrainingIDCA(Training):
             Loss Value for the step
 
         """
-        self.best_model.zero_grad()
-        output = self.best_model(data)
+        self.current_model.zero_grad()
+        output = self.current_model(data)
         loss_val = self.loss(output, target)
         loss_val.backward()
         self.optimizer.step()
         return loss_val.item()
-
-    def train_full(self, dataset):
-        """
-        One training iteration, goes through the entire dataset
-
-        Parameters
-        ----------
-        trainset : torch.utils.data.Dataloader
-            The training dataset.
-
-        """
-        trainset = dataset.get_trainset(self.batch_size, self.shuffle)
-        for epoch in range(self.rounds):
-            epoch_loss = 0.0
-            count = 0
-            for data, target in trainset:
-                logging.debug(
-                    "Starting minibatch {} with num_samples: {}".format(
-                        count, len(data)
-                    )
-                )
-                logging.debug("Classes: {}".format(target))
-                epoch_loss += self.trainstep(data, target)
-                count += 1
-            logging.debug("Epoch: {} loss: {}".format(epoch, epoch_loss / count))
-
-    def train(self, dataset: Dataset, treshold: float = 0.0):
-        """
-        One training iteration
-
-        Parameters
-        ----------
-        dataset : decentralizepy.datasets.Dataset
-            The training dataset. Should implement get_trainset(batch_size, shuffle)
-        treshold : float, optional
-            Treshold in [0, 1] to explore the space. If set to 0, no exploration is done.
-
-        """
-
-        # chose the best model
-        # self.choose_best_model(dataset)
-        self.choose_best_model_explore(dataset, treshold=treshold)
-
-        # reset the optimizer
-        self.reset_optimizer(
-            self.optimizer_class(self.best_model.parameters(), **self.optimizer_params)
-        )
-
-        self.best_model.train()  # set the best model to train mode
-
-        if self.full_epochs:
-            self.train_full(dataset)
-        else:
-            iter_loss = 0.0
-            count = 0
-            trainset = dataset.get_trainset(self.batch_size, self.shuffle)
-            while count < self.rounds:
-                for data, target in trainset:
-                    iter_loss += self.trainstep(data, target)
-                    count += 1
-                    logging.debug("Round: {} loss: {}".format(count, iter_loss / count))
-                    if count >= self.rounds:
-                        break
-
-    def choose_best_model(self, dataset: Dataset):
-        """
-        Choose the best model from the list of models
-
-        Parameters
-        ----------
-        dataset : decentralizepy.datasets.Dataset
-            The dataset interface.
-        """
-        self.models_losses = [self.eval_loss(model, dataset) for model in self.models]
-        self.best_model_loss = min(self.models_losses)
-        self.best_model_idx = self.models_losses.index(self.best_model_loss)
-        self.best_model = self.models[self.best_model_idx]
-
-    def choose_best_model_explore(self, dataset: Dataset, treshold: float = 0.1):
-        """
-        Choose the best model from the list of models.
-        Also, have a percentage of choosing a random model to explore the space.
-
-        Parameters
-        ----------
-        dataset : decentralizepy.datasets.Dataset
-            The dataset interface.
-        """
-        self.models_losses = [self.eval_loss(model, dataset) for model in self.models]
-        if random.uniform(0, 1) < treshold:
-            self.best_model_idx = random.randint(0, len(self.models) - 1)
-            self.best_model_loss = self.models_losses[self.best_model_idx]
-        else:
-            self.best_model_loss = min(self.models_losses)
-            self.best_model_idx = self.models_losses.index(self.best_model_loss)
-        self.best_model = self.models[self.best_model_idx]
 
     def get_current_best_model(self) -> Model:
         """
@@ -240,9 +240,9 @@ class TrainingIDCA(Training):
             Best model
 
         """
-        return self.best_model
+        return self.current_model
 
-    def get_best_model_loss(self) -> float:
+    def get_current_model_loss(self) -> float:
         """
         Get the loss of the best model
 
@@ -252,9 +252,10 @@ class TrainingIDCA(Training):
             Loss of the best model
 
         """
-        return self.best_model_loss
+        assert self.current_model_loss is not None
+        return self.current_model_loss
 
-    def get_models_losses(self) -> List[float]:
+    def get_all_models_loss(self) -> List[float]:
         """
         Get the losses of all models
 
@@ -264,4 +265,5 @@ class TrainingIDCA(Training):
             Losses of all models
 
         """
+        assert self.models_losses is not None
         return self.models_losses
